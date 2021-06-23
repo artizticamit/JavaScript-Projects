20df415ddaaff06a958a8389920028b80fdd2efb
Artistic Amit <amit6539101@gmail.com>
2021-06-23 12:56:07 +0530

    adding color-flipper and empty scroll folder

---
+++ color-flipper/app.js
@@ -0,0 +1,27 @@
+const colors = ['green', 'red', 'blue', 'greenyellow', 'tomato','gold','aqua','silver'];
+// function change(){
+//     let btn = document.getElementById('click');
+//     let bg = document.getElementById('background');
+//     if(bg.style.display != 'none')
+//     {
+//         bg.style.display  = 'none';
+//     }
+//     else{
+//         bg.style.display = 'block';
+//     }
+// }
+
+const btn = document.getElementById('click');
+const color = document.querySelector('#show-color');
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+
+function getRandomNumber(){
+    let num = Math.floor(Math.random()*colors.length);
+    return num;
+}
